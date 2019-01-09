70e075e9ffedeb58a06e59b6350783f23b16bf2f
LeoCeasar <1304697749@qq.com>
2019-01-09 23:23:06 +0800

    fix a miner bug,about linux shell commend about 'cd'.

--- sh/mkcd.sh
+++ sh/mkcd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+#因为这是shell脚本原先想实现的目的是通过cd 命令行在终端进行目录的切换，但是好像不能输出， 所以添加一个命令 alias mkcd=". ./mkcd.sh" 然后在终端使用mkcd可以达到目的
 
 if test -z "$1";
 then
     echo "error command"
     exit 0
 fi
-echo `mkdir $1`
-
-echo `cd "./"$1`
+mkdir $1
+cd $1
