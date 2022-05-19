56cd12ae65a672361a95ecda12733c5aa02de08a
LeoCeasar <1304697749@qq.com>
2022-05-19 01:43:08 +0100

    携程题目提交，题目不清晰

---
+++ C/mod.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+	int test = 213;
+	int a = 0;
+	int b = 0;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	if ( a % b > 0){
+		printf("0000");
+	}
+	printf("%d\n",1111);
+	printf("%d\n",1%3);
+	printf("\n");
+	printf("test\n %d \n %d \n", a, b);
+}
